2becdd4aa098be783dd88379e0714c626320d4c4
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2023-12-01 11:27:10 +0100

    Solve day 1 part 2

--- 01.rb
+++ 01.rb
@@ -9,4 +9,28 @@ class Day01 < Common
       [fst.captures.first, snd.captures.first].join.to_i
     end
   end
+
+  def part2
+    lines.sum do |line|
+      fst = /(one|two|three|four|five|six|seven|eight|nine|ten|\d).*$/.match(line)
+      snd = /^.*(one|two|three|four|five|six|seven|eight|nine|ten|\d)/.match(line)
+      [to_num(fst.captures.first), to_num(snd.captures.first)].join.to_i
+    end
+  end
+
+  NUMBERS = {
+    'one' => 1,
+    'two' => 2,
+    'three' => 3,
+    'four' => 4,
+    'five' => 5,
+    'six' => 6,
+    'seven' => 7,
+    'eight' => 8,
+    'nine' => 9
+  }.freeze
+
+  def to_num(str)
+    NUMBERS[str] || str.to_i
+  end
 end
