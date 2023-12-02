1ff79ce44707c1a21db2d92a9e908dc785910673
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2023-12-02 09:08:03 +0100

    Solve day 2 part 2

--- 02.rb
+++ 02.rb
@@ -32,4 +32,25 @@ class Day02 < Common
 
     (games - invalid).sum
   end
+
+  def part2
+    games = Hash.new { |hash, key| hash[key] = { red: 0, green: 0, blue: 0 } }
+
+    lines.each do |line|
+      game = /Game (?<game>\d+):/.match(line).match(:game).to_i
+
+      rest = line.gsub(/Game (?<game>\d+):/, '')
+      rest.split(';').each do |record|
+        red = /(\d+) red/.match(record)&.match(1).to_i || 0
+        green = /(\d+) green/.match(record)&.match(1).to_i || 0
+        blue = /(\d+) blue/.match(record)&.match(1).to_i || 0
+
+        games[game][:red] = red if red > games[game][:red]
+        games[game][:green] = green if green > games[game][:green]
+        games[game][:blue] = blue if blue > games[game][:blue]
+      end
+    end
+
+    games.sum { |_, game| game[:red] * game[:green] * game[:blue] }
+  end
 end
