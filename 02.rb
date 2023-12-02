e02aa4731ee321c3710605cecd95a035663064c5
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2023-12-02 10:05:51 +0100

    Refactor day 2 part 1

--- 02.rb
+++ 02.rb
@@ -11,26 +11,18 @@ class Day02 < Common
 
   def part1
     games = Set.new
-    invalid = Set.new
 
     lines.each do |line|
-      game = /Game (?<game>\d+):/.match(line).match(:game).to_i
-      games.add(game)
+      game_id, game_data = parse_game_line(line)
+      games.add(game_id)
 
-      rest = line.gsub(/Game (?<game>\d+):/, '')
-      rest.split(';').each do |record|
-        red = /(\d+) red/.match(record)&.match(1).to_i || 0
-        green = /(\d+) green/.match(record)&.match(1).to_i || 0
-        blue = /(\d+) blue/.match(record)&.match(1).to_i || 0
-
-        invalid.add(game) if red + blue + green > 39
-        invalid.add(game) if red > MAX[:red]
-        invalid.add(game) if green > MAX[:green]
-        invalid.add(game) if blue > MAX[:blue]
+      game_data.each do |record|
+        cubes = extract_cubes(record)
+        games.delete(game_id) if invalid_game?(cubes)
       end
     end
 
-    (games - invalid).sum
+    games.sum
   end
 
   def part2
@@ -52,5 +44,24 @@ class Day02 < Common
     end
 
     games.sum { |_, game| game[:red] * game[:green] * game[:blue] }
+  end
+
+  private
+
+  def parse_game_line(line)
+    game_id = line[/Game (\d+):/, 1].to_i
+    game_data = line.split(':')[1].split(';')
+    [game_id, game_data]
+  end
+
+  def extract_cubes(record)
+    colors = %i[red green blue]
+    cubes = colors.map { |color| [color, record[/(\d+) #{color}/, 1].to_i] }.to_h
+    cubes.default_proc = proc { 0 }
+    cubes
+  end
+
+  def invalid_game?(cubes)
+    cubes.any? { |color, count| count > MAX[color] } || cubes.values.sum > 39
   end
 end
