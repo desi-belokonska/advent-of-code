9c5f9e411f34b38968df8b43251a466d8ca181f6
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2023-12-02 09:22:42 +0100

    Refactor day 1 part 1

--- 01.rb
+++ 01.rb
@@ -4,9 +4,9 @@ require './common'
 class Day01 < Common
   def part1
     lines.sum do |line|
-      fst = /^\D*(\d)/.match(line)
-      snd = /(\d)\D*$/.match(line)
-      [fst.captures.first, snd.captures.first].join.to_i
+      first_digit = line[/\d/]
+      last_digit = line.reverse[/\d/]
+      "#{first_digit}#{last_digit}".to_i
     end
   end
 
