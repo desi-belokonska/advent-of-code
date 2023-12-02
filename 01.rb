01046c9817a3da6ba44ccb18051890d51d3b36ec
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2023-12-02 09:32:39 +0100

    Refactor day 1 part 2

--- 01.rb
+++ 01.rb
@@ -2,23 +2,7 @@ require './common'
 
 # Day 1 of Advent of code
 class Day01 < Common
-  def part1
-    lines.sum do |line|
-      first_digit = line[/\d/]
-      last_digit = line.reverse[/\d/]
-      "#{first_digit}#{last_digit}".to_i
-    end
-  end
-
-  def part2
-    lines.sum do |line|
-      fst = /(one|two|three|four|five|six|seven|eight|nine|ten|\d).*$/.match(line)
-      snd = /^.*(one|two|three|four|five|six|seven|eight|nine|ten|\d)/.match(line)
-      [to_num(fst.captures.first), to_num(snd.captures.first)].join.to_i
-    end
-  end
-
-  NUMBERS = {
+  DIGIT_MAP = {
     'one' => 1,
     'two' => 2,
     'three' => 3,
@@ -30,7 +14,35 @@ class Day01 < Common
     'nine' => 9
   }.freeze
 
-  def to_num(str)
-    NUMBERS[str] || str.to_i
+  def part1
+    lines.sum do |line|
+      first_digit = line[/\d/]
+      last_digit = line.reverse[/\d/]
+      "#{first_digit}#{last_digit}".to_i
+    end
+  end
+
+  def part2
+    lines.sum do |line|
+      first_digit = find_digit(line, :first)
+      last_digit = find_digit(line, :last)
+      "#{first_digit}#{last_digit}".to_i
+    end
+  end
+
+  private
+
+  def find_digit(str, position)
+    regex = if position == :first
+              /(one|two|three|four|five|six|seven|eight|nine|ten|\d).*$/
+            else
+              /^.*(one|two|three|four|five|six|seven|eight|nine|ten|\d)/
+            end
+    match = regex.match(str)
+    convert_to_digit(match.captures.first)
+  end
+
+  def convert_to_digit(str)
+    DIGIT_MAP[str] || str.to_i
   end
 end
