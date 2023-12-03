937d954af168d3a707661187cb577ea7d139137b
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2023-12-03 16:58:20 +0100

    Refactor day 3 part 2

--- 03.rb
+++ 03.rb
@@ -4,7 +4,7 @@ require './common'
 class Day03 < Common
   def part1
     lines.each_with_index.flat_map do |line, row|
-      numbers_with_positions(line).filter_map do |num, start_col, end_col|
+      find_numbers_with_positions(line).filter_map do |num, start_col, end_col|
         num if adjacent_to_symbol?(row, start_col, end_col)
       end
     end.sum
@@ -15,16 +15,16 @@ class Day03 < Common
     gears = Hash.new { |hash, key| hash[key] = [] }
 
     lines.each_with_index do |line, row|
-      numbers_with_positions(line).each do |num, start_col, end_col|
-        gears_around([row, start_col, end_col]).each do |gear_position|
-          gears[gear_position].push(num)
+      find_numbers_with_positions(line).each do |num, start_col, end_col|
+        find_adjacent_gears(row, start_col, end_col).each do |pos|
+          gears[pos].push(num)
         end
       end
     end
+
     gears
       .values
-      .filter { |nums| nums.length == 2 }
-      .map { |nums| nums.inject(&:*) }
+      .filter_map { |nums| nums.inject(&:*) if nums.length == 2 }
       .sum
   end
 
@@ -35,7 +35,7 @@ class Day03 < Common
   end
 
   # returns numbers and their position as [number, start_col, end_col]
-  def numbers_with_positions(line)
+  def find_numbers_with_positions(line)
     line.enum_for(:scan, /\d+/).map do
       match = Regexp.last_match
       [match[0].to_i, match.begin(0), match.end(0) - 1]
@@ -47,23 +47,18 @@ class Day03 < Common
   end
 
   # returns array of gear positions [row, col]
-  def gears_around(position)
-    row, colstart, colend = position
+  def find_adjacent_gears(row, start_col, end_col)
     gears = []
 
-    # up
-    grid.at(row - 1)&.each_with_index do |ch, i|
-      gears.push([row - 1, i]) if ((colstart - 1)..(colend + 1)).include?(i) && (ch == '*')
-    end
+    (-1..1).each do |dx|
+      check_row = row + dx
+      next unless check_row.between?(0, grid.length - 1)
 
-    # current line
-    grid.at(row)&.each_with_index do |ch, i|
-      gears.push([row, i]) if ((colstart - 1)..(colend + 1)).include?(i) && (ch == '*')
-    end
+      (start_col - 1..end_col + 1).each do |check_col|
+        next unless check_col.between?(0, row_length - 1)
 
-    # down
-    grid.at(row + 1)&.each_with_index do |ch, i|
-      gears.push([row + 1, i]) if ((colstart - 1)..(colend + 1)).include?(i) && (ch == '*')
+        gears.push([check_row, check_col]) if grid[check_row][check_col] == '*'
+      end
     end
 
     gears
@@ -89,19 +84,5 @@ class Day03 < Common
 
   def symbol?(cell)
     !/\d|\./.match?(cell)
-  end
-
-  Array.class_eval do
-    def at(idx)
-      if idx.instance_of?(Range)
-        min, max = idx.minmax
-        min = 0 if min.negative?
-        idx = min..max
-      end
-
-      return nil if !idx.instance_of?(Range) && idx.negative?
-
-      self[idx]
-    end
   end
 end
