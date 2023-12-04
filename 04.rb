12469d3fa10e025b98843ae9fd62924c97131e3d
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2023-12-04 10:39:08 +0100

    Solve day 4 part 2

--- 04.rb
+++ 04.rb
@@ -10,6 +10,24 @@ class Day04 < Common
     end
   end
 
+  def part2
+    scratchcards = Hash.new { |hash, key| hash[key] = 1 }
+
+    lines.each do |line|
+      card_id = line[/Card\s+(\d+):/, 1].to_i
+      matching_num = matching_numbers(line).size
+
+      times_owned = scratchcards[card_id]
+
+      matching_num.times do |n|
+        next_card_id = card_id + n + 1
+        scratchcards[next_card_id] += times_owned if next_card_id <= lines.length
+      end
+    end
+
+    scratchcards.values.sum
+  end
+
   private
 
   def matching_numbers(line)
