634fb01ad4a20764ec69cd1d1148a1ddaca4c69d
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2023-12-06 10:51:04 +0100

    Solve day 6 part 1

---
+++ 06.rb
@@ -0,0 +1,26 @@
+require './common'
+
+# Day 6 of Advent of code
+class Day06 < Common
+  def part1
+    times = lines[0].scan(/\d+/).map(&:to_i)
+    distances = lines[1].scan(/\d+/).map(&:to_i)
+    times.zip(distances).map do |time, distance|
+      ways_to_win(time, distance)
+    end.inject(&:*)
+  end
+
+  def ways_to_win(time, distance_to_beat)
+    time_to_hold = 1
+    ways_to_win = 0
+
+    while time_to_hold < time
+      time_to_travel = time - time_to_hold
+      distance = time_to_hold * time_to_travel
+      ways_to_win += 1 if distance > distance_to_beat
+      time_to_hold += 1
+    end
+
+    ways_to_win
+  end
+end
