edd85d1e7cd2c66f71375d21ba28ffb38fb5deae
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2023-12-11 15:19:53 +0100

    Refactor day 11 part 1

--- 11.rb
+++ 11.rb
@@ -4,44 +4,34 @@ require './common'
 # Day 11 of Advent of code
 class Day11 < Common
   def part1
-    # aljdlkfs
-    # puts expanded_grid.map(&:join).join("\n")
-    # p galaxies
-
-    distances = Hash.new(Float::INFINITY)
-    p galaxies.length
-    galaxies.each do |g|
-      paths_from_g = bfs(g)
-      paths_from_g.each do |k, v|
-        distances[[g, k].to_set] = v if galaxies.include?(k) && k != g
-      end
-    end
-    distances.values.sum
+    galaxies.combination(2).map do |a, b|
+      manhattan(a, b)
+    end.sum
   end
 
   private
 
-  def grid
-    @grid ||= lines.map(&:chars)
+  def full_grid
+    @full_grid ||= lines.map(&:chars)
   end
 
-  def expanded_grid
+  def expanded_grid(scale = 2)
     @expanded_grid ||= proc {
       # expand grid by duplicating empty rows
-      new_grid = grid.flat_map do |row|
-        row.include?('#') ? [row] : [row, row]
+      new_grid = full_grid.flat_map do |row|
+        row.include?('#') ? [row] : Array.new(scale, row)
       end
 
       # expand grid by duplicating empty columns
       new_grid.transpose.flat_map do |col|
-        col.include?('#') ? [col] : [col, col]
+        col.include?('#') ? [col] : Array.new(scale, col)
       end.transpose
     }.call
   end
 
-  def galaxies
+  def galaxies(grid = expanded_grid)
     @galaxies ||= proc {
-      expanded_grid.flat_map.with_index do |row, i|
+      grid.flat_map.with_index do |row, i|
         row.filter_map.with_index do |col, j|
           [i, j] if col == '#'
         end
@@ -49,36 +39,7 @@ class Day11 < Common
     }.call
   end
 
-  def bfs(start)
-    p start
-    queue = [start]
-    distances = { start => 0 }
-    distances.default = Float::INFINITY
-    while queue.any?
-      current = queue.shift
-      neighbors(current).each do |neighbor|
-        if distances[neighbor] == Float::INFINITY
-          distances[neighbor] = distances[current] + 1
-          queue << neighbor
-        end
-      end
-    end
-    distances
-  end
-
-  def neighbors(pos)
-    row, col = pos
-    [
-      [row - 1, col],
-      [row + 1, col],
-      [row, col - 1],
-      [row, col + 1]
-    ].filter_map do |neighbor|
-      unless (0...expanded_grid.length).include?(neighbor[0]) && (0...expanded_grid[0].length).include?(neighbor[1])
-        next
-      end
-
-      neighbor
-    end
+  def manhattan(pos_a, pos_b)
+    (pos_a[0] - pos_b[0]).abs + (pos_a[1] - pos_b[1]).abs
   end
 end
