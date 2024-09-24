87e55f822c425bfecd6e6e58e4d3d3fd8c408b0b
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2024-09-24 15:20:17 +0200

    Solve day 12, part 2, 2015

--- 2015/12.rb
+++ 2015/12.rb
@@ -1,8 +1,29 @@
 require_relative '../common'
-require 'set'
+require 'json'
 
 class Day12 < Common
   def part1
     lines.first.scan(/-?\d+/).map(&:to_i).sum
+  end
+
+  def part2
+    json = JSON.parse!(lines.first)
+
+    sum(json)
+  end
+
+  private
+
+  def sum(json)
+    case json
+    when Hash
+      json.value?('red') ? 0 : sum(json.values)
+    when Array
+      json.map { |val| sum(val) }.sum
+    when Integer
+      json
+    else
+      0
+    end
   end
 end
