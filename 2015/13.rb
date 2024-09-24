bf3bf22c5c3c2a87b66b710d5c7ad973e4abddff
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2024-09-24 16:15:02 +0200

    Solve day 13, part 2, 2015

--- 2015/13.rb
+++ 2015/13.rb
@@ -13,15 +13,40 @@ class Day13 < Common
       snd = match[4]
       units = match[2] == 'gain' ? match[3].to_i : - match[3].to_i
 
-      happiness[[fst, snd].to_set] += units
+      happiness[[fst, snd].sort] += units
       names.merge([fst, snd])
     end
 
     names.to_a.permutation.map do |ordering|
       sum = ordering.each_cons(2).sum do |pair|
-        happiness[pair.to_set]
+        happiness[pair.sort]
       end
-      sum + happiness[[ordering.first, ordering.last].to_set]
+      sum + happiness[[ordering.first, ordering.last].sort]
+    end.max
+  end
+
+  def part2
+    happiness = Hash.new { |hash, key| hash[key] = 0 }
+    names = Set.new
+
+    lines.each do |line|
+      match = line.match(/(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)./)
+
+      fst = match[1]
+      snd = match[4]
+      units = match[2] == 'gain' ? match[3].to_i : - match[3].to_i
+
+      happiness[[fst, snd].sort] += units
+      names.merge([fst, snd])
+    end
+
+    names.add('me')
+
+    names.to_a.permutation.map do |ordering|
+      sum = ordering.each_cons(2).sum do |pair|
+        happiness[pair.sort]
+      end
+      sum + happiness[[ordering.first, ordering.last].sort]
     end.max
   end
 end
