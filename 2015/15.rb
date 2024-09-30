f6eac1dd1e3812bca740e2e39d261d85ad8834d9
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2024-09-30 10:54:22 +0200

    Solve day 15, part 1, 2015

---
+++ 2015/15.rb
@@ -0,0 +1,61 @@
+require_relative '../common'
+
+class Day15 < Common
+  MAX = 10
+
+  def part1
+    names = ingredients.map { |ing| ing[:name] }
+    n = ingredients.length
+
+    permutations(n, 100, names).map do |perm|
+      total_score(ingredients, perm)
+    end.max
+  end
+
+  private
+
+  def ingredients
+    @ingredients ||= lines.map do |line|
+      match = line.match(/(\w+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)/)
+
+      name = match[1]
+      capacity = match[2].to_i
+      durability = match[3].to_i
+      flavor = match[4].to_i
+      texture = match[5].to_i
+      calories = match[6].to_i
+
+      { name:, capacity:, durability:, flavor:, texture:, calories: }
+    end
+  end
+
+  def total_score(ingredients, tsp)
+    capacity = ingredients.map { |ing| (ing[:capacity] * tsp[ing[:name]]) }.sum.clamp(0..)
+    durability = ingredients.map { |ing| (ing[:durability] * tsp[ing[:name]]) }.sum.clamp(0..)
+    flavor = ingredients.map { |ing| (ing[:flavor] * tsp[ing[:name]]) }.sum.clamp(0..)
+    texture = ingredients.map { |ing| (ing[:texture] * tsp[ing[:name]]) }.sum.clamp(0..)
+
+    capacity * durability * flavor * texture
+  end
+
+  def permutations(num, sum, names)
+    case num
+    when 2
+      (0..sum).flat_map do |a|
+        b = sum - a
+        names.zip([a, b]).permutation.map(&:to_h)
+      end.uniq
+    when 4
+      (0..sum).flat_map do |a|
+        (0..sum - a).flat_map do |b|
+          (0..sum - a - b).flat_map do |c|
+            d = sum - a - b - c
+            names.zip([a, b, c, d]).permutation.map(&:to_h)
+          end
+        end
+      end.uniq
+    else
+      []
+    end
+  end
+end
