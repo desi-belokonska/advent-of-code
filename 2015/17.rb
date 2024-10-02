91c01fcc01d8f099e84ca3027ccf7681dd9314a2
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2024-10-02 18:19:49 +0200

    Solve day 17, part 2, 2015

--- 2015/17.rb
+++ 2015/17.rb
@@ -7,6 +7,12 @@ class Day17 < Common
     all_combinations(containers).count { |comb| comb.sum == EGGNOG }
   end
 
+  def part2
+    fitting_containers = all_combinations(containers).filter { |comb| comb.sum == EGGNOG }
+    min_number = fitting_containers.min_by(&:length).length
+    fitting_containers.count { |comb| comb.length == min_number }
+  end
+
   private
 
   def containers
