9aed7e63c63c6e6eeb5346f29f240057dd09edf4
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2024-10-03 11:30:19 +0200

    Solve day 18, part 2, 2015

--- 2015/18.rb
+++ 2015/18.rb
@@ -2,34 +2,53 @@ require_relative '../common'
 
 class Day18 < Common
   def part1
-    # puts 'Initial state:'
-    # print_lights(lights)
+    current_lights = initial_lights
 
-    current_lights = lights
-
-    100.times do |_i|
-      # puts "\nAfter #{i + 1} step:"
-
+    100.times do
       current_lights = current_lights.each_with_index.map do |row, r|
         row.each_with_index.map do |light, c|
-          n = neighbors_on(r, c, current_lights).length
-          if light == 1 && ![2, 3].include?(n)
-            0
-          elsif light == 0 && n == 3
-            1
-          else
-            light
-          end
+          light_state(light, neighbors_on(r, c, current_lights))
         end
       end
-
-      # print_lights(current_lights)
     end
-    # print_lights(current_lights)
+
+    current_lights.flatten.sum
+  end
+
+  def part2
+    current_lights = initial_lights
+    fix_lights(current_lights)
+
+    100.times do
+      current_lights = current_lights.each_with_index.map do |row, r|
+        row.each_with_index.map do |light, c|
+          next 1 if fixed_lights.include?([r, c])
+
+          light_state(light, neighbors_on(r, c, current_lights))
+        end
+      end
+    end
+
     current_lights.flatten.sum
   end
 
   private
+
+  def initial_lights
+    @initial_lights ||= lines.map do |line|
+      line.split('').map { |ch| ch == '#' ? 1 : 0 }
+    end
+  end
+
+  def fixed_lights
+    @fixed_lights ||= [[0, 0], [0, initial_lights.first.length - 1], [initial_lights.length - 1, 0], [initial_lights.length - 1, initial_lights.first.length - 1]]
+  end
+
+  def fix_lights(lights)
+    fixed_lights.each do |(r, c)|
+      lights[r][c] = 1
+    end
+  end
 
   def print_lights(lights)
     lights.each do |row|
@@ -37,15 +56,19 @@ class Day18 < Common
     end
   end
 
-  def lights
-    @lights ||= lines.map do |line|
-      line.split('').map { |ch| ch == '#' ? 1 : 0 }
-    end
-  end
-
   def neighbors_on(row, col, lights)
     rows = [row - 1, row, row + 1].filter { |r| r >= 0 && r < lights.length }
     cols = [col - 1, col, col + 1].filter { |c| c >= 0 && c < lights.first.length }
-    rows.product(cols).filter { |(r, c)| lights[r][c] == 1 && [r, c] != [row, col] }
+    rows.product(cols).filter { |(r, c)| lights[r][c] == 1 && [r, c] != [row, col] }.length
+  end
+
+  def light_state(light, neighbors)
+    if light == 1 && ![2, 3].include?(neighbors)
+      0
+    elsif light == 0 && neighbors == 3
+      1
+    else
+      light
+    end
   end
 end
