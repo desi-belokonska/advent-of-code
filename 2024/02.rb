622aa491e13a281dcd51e43674ed7ffcd6d4a9ec
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2024-12-03 20:57:00 +0100

    day 2, part 2; 2024

--- 2024/02.rb
+++ 2024/02.rb
@@ -14,4 +14,26 @@ class Day02 < Common
       safely_increasing || safely_decreasing
     end
   end
+
+  def part2
+    lines.filter_map.count do |line|
+      report = line.split(' ').map(&:to_i)
+
+      (0..report.length).any? do |i|
+        report_dup = report.dup
+        report_dup.delete_at(i)
+
+        report_distances = report_dup.each_cons(2).map { |a, b| b - a }
+
+        safely_increasing = report_distances.all? do |level|
+          level.positive? && level.abs >= 1 && level.abs <= 3
+        end
+        safely_decreasing = report_distances.all? do |level|
+          level.negative? && level.abs >= 1 && level.abs <= 3
+        end
+
+        safely_increasing || safely_decreasing
+      end
+    end
+  end
 end
