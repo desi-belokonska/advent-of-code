07c7273b689d7df9a02d30f8ff225b5ea8c8fb16
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2024-12-03 12:24:40 +0100

    Solve day 1, part 2, 2024

--- 2024/01.rb
+++ 2024/01.rb
@@ -8,4 +8,13 @@ class Day01 < Common
 
     lft.zip(rgt).map { |a, b| (a - b).abs }.sum
   end
+
+  def part2
+    lft, rgt = lines.map { |line| line.split(' ').map(&:to_i) }
+                    .transpose
+
+    lft.sum(0) do |n|
+      n * rgt.count { |m| m == n }
+    end
+  end
 end
