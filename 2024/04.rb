dcfdeb23fd603f913f6ecb288069b9dd49f7280c
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2024-12-08 10:03:03 +0100

    day 4; part 2; 2024

--- 2024/04.rb
+++ 2024/04.rb
@@ -5,7 +5,23 @@ class Day04 < Common
     horizontals + verticals + diagonals
   end
 
+  def part2
+    rows.times.sum do |r|
+      cols.times.count do |c|
+        xmas?(r, c) if lines[r][c] == 'A'
+      end
+    end
+  end
+
   private
+
+  def rows
+    @rows ||= lines.length
+  end
+
+  def cols
+    @cols ||= lines[0].length
+  end
 
   def horizontals
     lines.sum do |line|
@@ -27,8 +43,6 @@ class Day04 < Common
 
   def diagonals
     total = 0
-    rows = lines.length
-    cols = lines[0].length
 
     # all possible diagonals
     (0...rows + cols - 1).each do |d|
@@ -48,5 +62,15 @@ class Day04 < Common
     end
 
     total
+  end
+
+  def xmas?(r, c)
+    return false if [r + 1, r, r - 1].any? { |n| n < 0 || n >= rows }
+    return false if [c + 1, c, c - 1].any? { |n| n < 0 || n >= cols }
+
+    diag_a = [lines[r - 1][c - 1], lines[r][c], lines[r + 1][c + 1]].join
+    diag_b = [lines[r - 1][c + 1], lines[r][c], lines[r + 1][c - 1]].join
+
+    %w[MAS SAM].include?(diag_a) && %w[MAS SAM].include?(diag_b)
   end
 end
